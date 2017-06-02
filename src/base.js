21b5be40669f311bdf8e377a17bcf88522ed9da3
Russell Long <russelllongj@gmail.com>
2017-06-02 14:05:59 +1000

    Moved code into functions. Created class for the spinner

--- src/base.js
+++ src/base.js
@@ -1,18 +1,71 @@
-var tickrate = 100
-var angle = 0
-var speed = 0
-var click_increment = 1
+var tickrate = 60;
+var angle = 0;
+var speed = 0;
+var decay = 0.1;
+var max_speed = 10;
+var click_increment = 1;
+var all_spinners = new Array();
+var spinner = null;
 
-$(document).ready(function(){
-	console.log("Document loaded")
+
+function Spinner(start_speed, decay, max_speed){
+	this.speed = start_speed;
+	this.decay = decay;
+	this.max_speed = max_speed;
+	this.angle = 0;
+}
+
+Spinner.prototype.changeSpeed = function(amount){
+	this.speed += amount;
+
+	if(this.speed > this.max_speed){
+		this.speed = this.max_speed;
+	}
+	else if(this.speed < 0){
+		this.speed = 0;
+	}
+	
+}
+
+Spinner.prototype.update = function(){
+	this.changeSpeed(-this.decay);
+	this.angle += this.speed;
+}
+
+$(document).ready(game_setup);
+
+
+function game_setup(){
+	clicks_setup();
+	create_objects();
+	game_loop();
+}
+
+function clicks_setup(){
+
+	console.log("Document loaded");
 	$("#spinner").click(function(){
-		speed += click_increment;
+		spinner.changeSpeed(click_increment);
 	});
 
+}
+
+function create_objects(){
+	spinner = new Spinner(0, 0.1, 10)
+	all_spinners.push(spinner);
+}
+
+function game_loop(){
 	window.setInterval(function(){
-		angle += speed
-		$("#spinner").rotate(angle);
-
+		spinner.update();
+		$("#spinner").rotate(spinner.angle);
 	}, tickrate);
+}
 
-});
+function increase(base_number, amount){
+	base_number += amount;
+	base_number = base_number.toFixed(2);
+	return base_number;
+}
+
+game_setup()
